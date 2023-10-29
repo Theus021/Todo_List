b511c08ecfa9fb664438a93b52b1edd63018401f
Thues021 <mateusoliver2109@hotmail.com>
2023-10-29 09:52:13 -0300

    adding_edit_task_functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,73 +1,111 @@
-const todoForm = document.querySelector('#todo-form');
-const todoInput = document.querySelector('#todo-input');
-const todoList = document.querySelector('#todo-list');
-const editForm = document.querySelector('#edit-form');
-const editInput = document.querySelector('#edit-input');
-const cancelInput = document.querySelector('#cancel-edit-btn');
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelInput = document.querySelector("#cancel-edit-btn");
+
+let oldInputValue;
 
 // funcoes
 
 const saveTodo = (text) => {
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
 
-    const todo = document.createElement('div')
-    todo.classList.add('todo')
+  const todoTitle = document.createElement("h3");
+  todoTitle.innerText = text;
+  todo.appendChild(todoTitle);
 
-    const todoTitle = document.createElement('h3')
-    todoTitle.innerText = text;
-    todo.appendChild(todoTitle) 
-    
-    const doneBtn = document.createElement('button')
-    doneBtn.classList.add('finish-todo')
-    doneBtn.innerHTML ='<i class="fa-solid fa-check"></i>'
-    todo.appendChild(doneBtn)
+  const doneBtn = document.createElement("button");
+  doneBtn.classList.add("finish-todo");
+  doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+  todo.appendChild(doneBtn);
 
-    const editBtn = document.createElement('button')
-    editBtn.classList.add('edit-todo')
-    editBtn.innerHTML ='<i class="fa-solid fa-pen"></i>'
-    todo.appendChild(editBtn)
-    
-    const removeBtn = document.createElement('button')
-    removeBtn.classList.add('remove-todo')
-    removeBtn.innerHTML ='<i class="fa-solid fa-xmark"></i>'
-    todo.appendChild(removeBtn)
-    
-    todoList.appendChild(todo)
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-todo");
+  editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+  todo.appendChild(editBtn);
 
-    todoInput.value = '';
-    todoInput.focus();
-}
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("remove-todo");
+  removeBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  todo.appendChild(removeBtn);
+
+  todoList.appendChild(todo);
+
+  todoInput.value = "";
+  todoInput.focus();
+};
 
 const toggleForms = () => {
-    editForm.classList.toggle('hide')
-    todoForm.classList.toggle('hide')
-    todoList.classList.toggle('hide')
-}
+  editForm.classList.toggle("hide");
+  todoForm.classList.toggle("hide");
+  todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+  const todos = document.querySelectorAll(".todo");
+
+  todos.forEach((todos) => {
+    let todoTitle = todos.querySelector("h3");
+
+    if (todoTitle.innerText === oldInputValue) {
+      todoTitle.innerText = text;
+    }
+  });
+};
 
 //eventos
-todoForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    
-    const inputValue = todoInput.value; 
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    if(inputValue != null) {
-        saveTodo(inputValue)
-    }
-} )
+  const inputValue = todoInput.value;
 
+  if (inputValue) {
+    saveTodo(inputValue);
+  }
+});
 
-document.addEventListener('click', (e) => {
-    const targetElement = e.target;
-    const parentElement = targetElement.closest('div')
+document.addEventListener("click", (e) => {
+  const targetElement = e.target;
+  const parentElement = targetElement.closest("div");
+  let todoTitle;
 
-    if(targetElement.classList.contains('finish-todo')){
-        parentElement.classList.toggle('done')
-    }
+  if (parentElement && parentElement.querySelector("h3")) {
+    todoTitle = parentElement.querySelector("h3").innerText;
+  }
 
-    if(targetElement.classList.contains('remove-todo')){
-        parentElement.remove()
-    }
+  if (targetElement.classList.contains("finish-todo")) {
+    parentElement.classList.toggle("done");
+  }
 
-    if(targetElement.classList.contains('edit-todo')){
-        toggleForms()
-    }
-})
+  if (targetElement.classList.contains("remove-todo")) {
+    parentElement.remove();
+  }
+
+  if (targetElement.classList.contains("edit-todo")) {
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
+    toggleForms();
+  }
+});
+
+editForm.addEventListener("submit", (e) => {
+
+  e.preventDefault();
+
+  const editInputValue = editInput.value;
+
+  if (editInputValue) {
+    updateTodo(editInputValue);
+  }
+
+  toggleForms();
+
+});
+
+cancelInput.addEventListener("click", (e) => {
+  e.preventDefault();
+  toggleForms();
+});
